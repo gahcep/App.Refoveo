a0255504a30f3eccd91dd706d01bf00e335daf3e
Sergey Danielyan <sdanielyan@ebosphorus.com>
2014-09-20 02:13:54 +1100

    Update version

--- App.Refoveo/Properties/GlobalAssemblyInfo.cs
+++ App.Refoveo/Properties/GlobalAssemblyInfo.cs
@@ -38,6 +38,6 @@
  *   
  *  {develop} branch contains ONLY alpha version
  */
-[assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
-[assembly: AssemblyInformationalVersion("0.4.0-alpha.5")]
+[assembly: AssemblyVersion("0.5.0")]
+[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyInformationalVersion("0.5.0-alpha.6")]
