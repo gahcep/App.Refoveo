8f6c591735e18f83ffafb2250b1c554e736ff308
Sergey Danielyan <sdanielyan@ebosphorus.com>
2014-10-03 00:07:48 +1100

    Skeleton implementation for all FeedSource classes

--- App.Refoveo/FeedSources/HttpsSource.cs
+++ App.Refoveo/FeedSources/HttpsSource.cs
@@ -1,8 +1,48 @@
-﻿using App.Refoveo.Abstractions;
+﻿using System;
+using System.IO;
+using App.Refoveo.Abstractions;
+using App.Refoveo.Shared;
 
 namespace App.Refoveo.FeedSources
 {
     public class HttpsSource : IFeedSource
     {
+        /* Returns its FeedSource type */
+
+        public FeedSourceType IdentifyItself()
+        {
+            return FeedSourceType.FeedSourceHttps;
+        }
+
+        /* Returns appcast.xml contents */
+
+        public string AppcastAsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemoryStream AppcastAsStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        /* Returns appversion.xml */
+
+        public string AppversionAsString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MemoryStream AppversionAsStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        /* Sets whether compression is used or not */
+
+        public void CompressionUsed(bool forAppcast, bool forAppversion)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
